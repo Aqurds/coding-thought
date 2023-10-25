0b5bbdcf2c68874485b305bdee2ca3b2b8605878
aqurds <omarf1320@gmail.com>
2023-10-25 16:37:23 +0600

    Update db based on review request v2

--- db/migrate/20231025085325_create_posts.rb
+++ db/migrate/20231025085325_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
-      t.references :author, foreign_key: true, index: true
+      t.references :author, foreign_key: { to_table: 'users' }
       t.string :title
       t.text :text
       t.datetime :created_at
