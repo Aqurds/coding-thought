eb0275c0877b4405c24bfc8f7537ff10dd32c049
aqurds <omarf1320@gmail.com>
2023-10-27 17:01:26 +0600

    Applied review request changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
 
-  belongs_to :user, foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User'
 
   def self.count_posts(user)
     user.update(postCounter: user.posts.size)
