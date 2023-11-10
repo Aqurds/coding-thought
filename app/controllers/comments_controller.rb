50962a4b7bf500721ccb4b7d344b953bebd33fe9
aqurds <omarf1320@gmail.com>
2023-11-10 15:17:18 +0600

    Fixed linter errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,19 @@
 class CommentsController < ApplicationController
-  # The new action is responsible for rendering the form to create a new comment. It retrieves the corresponding post based on the post_id parameter, creates a new comment instance, and sets @user to the @current_user. It's assumed that @current_user is set elsewhere in the application, likely through user authenticatio
+  # The new action is responsible for rendering the form to create a new comment.
+  # It retrieves the corresponding post based on the post_id parameter,
+  # creates a new comment instance, and sets @user to the @current_user.
+  # It's assumed that @current_user is set elsewhere in the application, likely through user authenticatio
   def new
     @post = Post.find(params[:post_id])
     @comment = Comment.new
     @user = @current_user
   end
 
-  # The create action is responsible for handling the form submission to create a new comment. It first finds the user based on the user_id parameter, then finds the corresponding post based on the post_id parameter. It then initializes a new comment associated with the post and the user, using the comment_params method to permit only the necessary parameters (in this case, just :text).
+  # The create action is responsible for handling the form submission to create a new comment.
+  # It first finds the user based on the user_id parameter,
+  # then finds the corresponding post based on the post_id parameter.
+  # It then initializes a new comment associated with the post and the user,
+  # using the comment_params method to permit only the necessary parameters (in this case, just :text).
   def create
     @user = User.find(params[:user_id]) # Get the user from params
     @post = Post.find(params[:post_id])
@@ -21,7 +28,9 @@ class CommentsController < ApplicationController
 
   private
 
-  # The comment_params method is a private method that uses Rails' strong parameters to whitelist and permit only the :text parameter from the submitted form data. This helps protect against mass assignment vulnerabilities.
+  # The comment_params method is a private method that uses Rails' strong parameters
+  # to whitelist and permit only the :text parameter from the submitted form data.
+  # This helps protect against mass assignment vulnerabilities.
   def comment_params
     params.require(:comment).permit(:text)
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,15 @@
 class PostsController < ApplicationController
   def index
-    # This action retrieves all posts belonging to a specific user (specified by params[:user_id]) and assigns them to the @posts instance variable.
+    # This action retrieves all posts belonging to a specific user (specified by params[:user_id])
+    # and assigns them to the @posts instance variable.
     # It's commonly used for displaying a list of posts for a particular user on the index page.
     @posts = Post.where(author_id: params[:user_id])
     @users = User.where(id: params[:user_id])
   end
 
   def show
-    # This action finds a single post based on the id parameter in the request and assigns it to the @post instance variable.
+    # This action finds a single post based on the id parameter in the request
+    # and assigns it to the @post instance variable.
     # It's used for displaying the details of a specific post on its own page.
     @post = Post.find(params[:id])
   end
@@ -23,8 +25,10 @@ class PostsController < ApplicationController
   def create
     # This action is triggered when the form for creating a new post is submitted.
     # It sets the current user (assuming the set_current_user method is defined elsewhere).
-    # It builds a new post associated with the current user using the parameters from the form (permitted by post_params).
-    # It sets the initial values for comments_counter and likes_counter (assuming these are counters for comments and likes on the post).
+    # It builds a new post associated with the current user using the parameters
+    # from the form (permitted by post_params).
+    # It sets the initial values for comments_counter and likes_counter
+    # (assuming these are counters for comments and likes on the post).
     # If the post is successfully saved, it redirects to the show page for the newly created post with a success notice.
     # If the save fails, it renders the new template again, showing validation errors.
 
@@ -43,7 +47,9 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    # This is a private method that uses strong parameters to whitelist and permit only the :title and :text parameters for creating a post. It helps protect against mass assignment vulnerabilities.
+    # This is a private method that uses strong parameters to whitelist
+    # and permit only the :title and :text parameters for creating a post.
+    # It helps protect against mass assignment vulnerabilities.
     params.require(:post).permit(:title, :text)
   end
 end
