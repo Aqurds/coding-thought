d0b1d8e1ef79b5ed4848f6a1c4042f58b68d8dfd
aqurds <omarf1320@gmail.com>
2023-11-17 15:02:43 +0600

    Add devise v3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :models
+  devise_for :users
+  get '/sign_in', to: 'sessions#new', as: 'sign_in'
+  
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create]do
     resources :comments , only:[:index, :new, :create]
